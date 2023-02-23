26a1f407d42487a4212d18dd40d6050f8da31d7d
1yusupoveldar1 <1yusupoveldar1@gmail.com>
2023-02-23 14:13:38 +0200

    hello2

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -23,7 +23,7 @@ export const Header = () => {
       <Container maxWidth="lg">
         <div className={styles.inner}>
           <Link className={styles.logo} to="/">
-            <div>YUSUPOV Eldar BLOG</div>
+            <div>YUSUPOV BLOG</div>
           </Link>
           <div className={styles.buttons}>
             {isAuth ? (
